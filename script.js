42a2d2be6558280f0f73395e6fc9f4b325314464
Vânia Eloíza <eloizavania@gmail.com>
2025-01-23 11:37:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
 
 function showSlide(n) {
-    slides[currentSlide].style.display= 'none';
-    currentSlide = (n +slides.length) % slides.length;
-    slides[currentSlide].style.display = 'block';
+  slides[currentSlide].style.display = "none";
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].style.display = "block";
 }
 
 //Mostrar o próximo slide
 function nextSlide() {
-    show nextSlide(currentSlide + 1);
+  showSlide(currentSlide + 1);
 }
 
 //Mostrar o slide anterior
 function prevSlide() {
-    showSlide(currentSlide - 1);
+  showSlide(currentSlide - 1);
 }
 
 //Iniciar a apresentação automaticamente (a cada 15 segundos)
